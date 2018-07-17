a6ce8e804ebe164d4a2dd32349c0b1b815d11bb5
Kriandir <kriekvdmeulen@gmail.com>
2018-07-17 11:29:00 +0200

    cleaned up code

--- databaseTools/dbtools.py
+++ databaseTools/dbtools.py
@@ -11,8 +11,9 @@ import pandas as pd
 
 
 def access(engine,host,port,user,password,database):
-    # Access the database using sqlalchemy
-    global db
+    """ Access the database using sqlalchemy"""
+    # make db global in order to be used in GetPandaExtracted
+        global db
     db = tkp.db.Database(engine=engine, host=host, port=port,
                      user=user, password=password, database=database)
     db.connect()
@@ -25,59 +26,43 @@ def GetVarParams(session,dataset_id):
     VarParams = session.query(Varmetric,Runningcatalog).select_from(join(Varmetric,Runningcatalog)).filter(Runningcatalog.dataset_id == dataset_id).all()
     return VarParams
 
-def GetPandaExtracted(session,dataset_id,**kwargs):
+def GetPandaExtracted(session,dataset_id,tablelist):
+    """Function for extracting data in pandas format"""
+
+    # First we always get Runcat in order to be able to filter on dataset_id more easily
     x = session.query(Runningcatalog).filter(Runningcatalog.dataset_id == dataset_id)
     dx = pd.read_sql_query(x.statement,db.connection)
-    # here we rename entries in runcat to their proper names
     dx = dx.rename(index=str,columns={'id' : 'runcat'})
 
-    if kwargs is not None:
-        for key,value in kwargs.iteritems():
+    # loop through all the tables provided
+    for value in tablelist:
 
-            # if table name = extractedsource
-            if value.lower() == "extractedsource":
-                y = session.query(Extractedsource).join(Runningcatalog.xtrsrc).filter(Runningcatalog.dataset_id == dataset_id)
-                dy = pd.read_sql_query(y.statement,db.connection)
+        # if table name = extractedsource
+        if value.lower() == "extractedsource":
+            y = session.query(Extractedsource).join(Runningcatalog.xtrsrc).filter(Runningcatalog.dataset_id == dataset_id)
+            dy = pd.read_sql_query(y.statement,db.connection)
 
-            # if table name = varmetric also make sure to change id name to newsourceid to avoid merge conflicts
-            if value.lower() == "varmetric":
-                y = session.query(Varmetric).join(Runningcatalog).filter(Runningcatalog.dataset_id == dataset_id)
-                dy = pd.read_sql_query(y.statement,db.connection)
-                dy = dy.rename(index=str,columns={'id':'varmetric'})
+        # if table name = varmetric also make sure to change id name to varmetric
+        if value.lower() == "varmetric":
+            y = session.query(Varmetric).join(Runningcatalog).filter(Runningcatalog.dataset_id == dataset_id)
+            dy = pd.read_sql_query(y.statement,db.connection)
+            dy = dy.rename(index=str,columns={'id':'varmetric'})
 
-            if value.lower() == "newsource":
-                y = session.query(Newsource).join(Runningcatalog).filter(Runningcatalog.dataset_id == dataset_id)
-                dy = pd.read_sql_query(y.statement,db.connection)
-                dy = dy.rename(index=str,columns={'id':'newsource'})
-                dy = dy.rename(index=str,columns={'trigger_xtrsrc':'xtrsrc'})
+        # if table name = newsource also make sure to rename columns id and trigger_xtrsrc
+        if value.lower() == "newsource":
+            y = session.query(Newsource).join(Runningcatalog).filter(Runningcatalog.dataset_id == dataset_id)
+            dy = pd.read_sql_query(y.statement,db.connection)
+            dy = dy.rename(index=str,columns={'id':'newsource','trigger_xtrsrc':'xtrsrc'})
+
+        # if tablename = image make sure to rename id
+        if value.lower() == "image":
+            y = session.query(Image).filter(Image.dataset_id == dataset_id)
+            dy = pd.read_sql_query(y.statement,db.connection)
+            dy = dy.rename(index=str,columns={'id':'image'})
+
+        # check for same columns and then merge on those
+        same_col = dy.columns.intersection(dx.columns)
+        dx = pd.merge(dx,dy,on=list(same_col))
 
 
-            if value.lower() == "image":
-                y = session.query(Image).filter(Image.dataset_id == dataset_id)
-                dy = pd.read_sql_query(y.statement,db.connection)
-                dy = dy.rename(index=str,columns={'id':'image'})
-
-
-            # try:
-
-            same_col = dy.columns.intersection(dx.columns)
-            dx = pd.merge(dx,dy,on=list(same_col))
-            # except:
-            #     print "error no proper merge possible trying merge on id:"
-            #     try:
-            #         dx = pd.merge(dx,dy,on=['id'])
-            #     except:
-		    #         print "error no column id trying merge on runcat:"
-            #         dx = pd.merge(dx,dy,on=['runcat'])
-            # dy = []
-
-    # dx = pd.read_sql_query(x.statement,db.connection)
-    # dy = pd.read_sql_query(y.statement,db.connection)
-
-    # # here we rename entries in runcat to their proper names
-    # dx = dx.rename(index=str,columns={'id' : 'runcat','xtrsrc':'id'})
-    # # here we drop duplicate columns from runcat
-    # dx = dx.drop(columns = {'x','y','z','zone'})
-    # here we merge runcat and extractedsource
-    # merged = pd.merge(dx,dy,on=['id'])
     return dx

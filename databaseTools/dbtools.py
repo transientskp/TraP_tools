5e81f197ef844c54389fcef6b6599dcd9e5996c6
Kriandir <kriekvdmeulen@gmail.com>
2018-07-17 11:38:59 +0200

    made support for single table proper cleaned up code also made extra try except to catch wrong tables

--- databaseTools/dbtools.py
+++ databaseTools/dbtools.py
@@ -26,6 +26,39 @@ def GetVarParams(session,dataset_id):
     VarParams = session.query(Varmetric,Runningcatalog).select_from(join(Varmetric,Runningcatalog)).filter(Runningcatalog.dataset_id == dataset_id).all()
     return VarParams
 
+def MergeTables(session,dataset_id,value,dx):
+    # if table name = extractedsource
+    if value.lower() == "extractedsource":
+        y = session.query(Extractedsource).join(Runningcatalog.xtrsrc).filter(Runningcatalog.dataset_id == dataset_id)
+        dy = pd.read_sql_query(y.statement,db.connection)
+
+    # if table name = varmetric also make sure to change id name to varmetric
+    if value.lower() == "varmetric":
+        y = session.query(Varmetric).join(Runningcatalog).filter(Runningcatalog.dataset_id == dataset_id)
+        dy = pd.read_sql_query(y.statement,db.connection)
+        dy = dy.rename(index=str,columns={'id':'varmetric'})
+
+    # if table name = newsource also make sure to rename columns id and trigger_xtrsrc
+    if value.lower() == "newsource":
+        y = session.query(Newsource).join(Runningcatalog).filter(Runningcatalog.dataset_id == dataset_id)
+        dy = pd.read_sql_query(y.statement,db.connection)
+        dy = dy.rename(index=str,columns={'id':'newsource','trigger_xtrsrc':'xtrsrc'})
+
+    # if tablename = image make sure to rename id
+    if value.lower() == "image":
+        y = session.query(Image).filter(Image.dataset_id == dataset_id)
+        dy = pd.read_sql_query(y.statement,db.connection)
+        dy = dy.rename(index=str,columns={'id':'image'})
+
+    try:
+        # check for same columns and then merge on those
+        same_col = dy.columns.intersection(dx.columns)
+        dx = pd.merge(dx,dy,on=list(same_col))
+    except:
+        "please enter a valid table or add the table you want to dbtools.py"
+
+    return dx
+
 def GetPandaExtracted(session,dataset_id,tablelist):
     """Function for extracting data in pandas format"""
 
@@ -34,35 +67,12 @@ def GetPandaExtracted(session,dataset_id,tablelist):
     dx = pd.read_sql_query(x.statement,db.connection)
     dx = dx.rename(index=str,columns={'id' : 'runcat'})
 
-    # loop through all the tables provided
-    for value in tablelist:
-
-        # if table name = extractedsource
-        if value.lower() == "extractedsource":
-            y = session.query(Extractedsource).join(Runningcatalog.xtrsrc).filter(Runningcatalog.dataset_id == dataset_id)
-            dy = pd.read_sql_query(y.statement,db.connection)
-
-        # if table name = varmetric also make sure to change id name to varmetric
-        if value.lower() == "varmetric":
-            y = session.query(Varmetric).join(Runningcatalog).filter(Runningcatalog.dataset_id == dataset_id)
-            dy = pd.read_sql_query(y.statement,db.connection)
-            dy = dy.rename(index=str,columns={'id':'varmetric'})
-
-        # if table name = newsource also make sure to rename columns id and trigger_xtrsrc
-        if value.lower() == "newsource":
-            y = session.query(Newsource).join(Runningcatalog).filter(Runningcatalog.dataset_id == dataset_id)
-            dy = pd.read_sql_query(y.statement,db.connection)
-            dy = dy.rename(index=str,columns={'id':'newsource','trigger_xtrsrc':'xtrsrc'})
-
-        # if tablename = image make sure to rename id
-        if value.lower() == "image":
-            y = session.query(Image).filter(Image.dataset_id == dataset_id)
-            dy = pd.read_sql_query(y.statement,db.connection)
-            dy = dy.rename(index=str,columns={'id':'image'})
-
-        # check for same columns and then merge on those
-        same_col = dy.columns.intersection(dx.columns)
-        dx = pd.merge(dx,dy,on=list(same_col))
+    if isinstance(tablelist,list):
+        # loop through all the tables provided
+        for value in tablelist:
+            dx = MergeTables(session,dataset_id,value,dx)
+    else:
+        dx = MergeTables(session,dataset_id,tablelist,dx)
 
 
     return dx
